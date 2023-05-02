365b4fb867b121b1ffde77cfc819ee8c98a5b369
ReneKubax <therene21@gmail.com>
2023-05-02 16:23:26 -0600

    last changes

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,10 +1,12 @@
 import {writeFile} from 'fs/promises';
+import { NextResponse } from 'next/server';
 import path from 'path';
 
 export async function POST(request) {
-
-const data = await request.formData();
+ try {
+  const data = await request.formData();
 const file  = data.get('file');
+
 
 const bytes = await file.arrayBuffer();
 const buffer= Buffer.from(bytes);
@@ -16,8 +18,20 @@ writeFile(filePath, buffer);
 console.log("file uploaded to", filePath);
 
 
-    return new Response(JSON.stringify({
+    return new Response(
+      JSON.stringify({
       message: 'uploading file',
     }))
+ } catch (error) {
+  return NextResponse.json(
+   JSON.stringify({
+    message: 'No file uploaded',
+  }),
+  {
+    status: 400,
+  }
+  );
+ }
+
   }
   
--- src/app/page.js
+++ src/app/page.js
@@ -5,30 +5,41 @@ function HomePage() {
 
 const [file, setFile] = useState(null);
 
+const handleFileChange = (e) => {
+  setFile(e.target.files[0]);
+}
+
   return (
     <div>
       <form onSubmit={async (e) => {
         e.preventDefault();
         if(!file) return;
-
-        const form = new FormData();
+try {
+  const form = new FormData();
         form.set('file', file);
 
         //sending file to server 
         const res = await fetch('/api/upload',{
           method: 'POST',
           body: form
-        })
+        }) 
+
+if(res.ok){
+  console.log("file uploaded");
+}
+
         const data = await res.json();
         console.log(data);
+} catch (error) {
+  console.log(error);
+}
+        
       }}>
         <label>
           Upload File:
         </label>
         <input type="file" 
-        onChange={(e) => {
-          setFile(e.target.files[0]);
-        }} />
+        onChange={handleFileChange} />
         <button>Upload</button>
       </form>
     </div>
