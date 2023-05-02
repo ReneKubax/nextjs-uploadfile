b1634ff0c7ef2df8b44d519dbd223d952c2b1578
ReneKubax <therene21@gmail.com>
2023-05-01 23:46:36 -0600

    Upload function

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,27 @@
+"use client";
+import { useState } from "react";
+
 function HomePage() {
+
+const [file, setFile] = useState(null);
+
   return (
     <div>
-      <h1>Home Page</h1>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        if(!file) return;
+        console.log(file);
+        console.log('uploading file');
+      }}>
+        <label>
+          Upload File:
+        </label>
+        <input type="file" 
+        onChange={(e) => {
+          setFile(e.target.files[0]);
+        }} />
+        <button>Upload</button>
+      </form>
     </div>
   );
 }
